99ce83a0f2d6ff28c31599021b46bd99884187da
[buse duran] <buseduran500@gmail.com>
2023-09-02 23:22:41 +0300

    commands

--- app.js
+++ app.js
@@ -1,13 +1,47 @@
-import validator from 'validator'
-import chalk from 'chalk'
 
-const command = process.argv[2]
 
-if (command === 'add')
-{
-    console.log('Adding note!')
-}
-else if (command === 'remove')
-{
-    console.log('Removing note!')
-}
+const yargs = require('yargs')
+
+yargs.version('1.1.0')
+
+//Create add command
+yargs.command({
+    command: 'add',
+    describe: 'add a new note',
+    handler: function ()
+    {
+        console.log('Adding a new note!')
+    }
+})
+
+//Create remove command
+yargs.command({
+    command: 'remove',
+    describe: 'remove a note',
+    handler: function ()
+    {
+        console.log('Removing the note!')
+    }
+})
+
+//Create list command
+yargs.command({
+    command: 'list',
+    describe: 'list notes',
+    handler: function ()
+    {
+        console.log('Listing the notes!')
+    }
+})
+
+//Create read command
+yargs.command({
+    command: 'read'
+    describe: 'read a note',
+    handler: function ()
+    {
+        console.log('Reading the notes!')
+    }
+})
+
+yargs.parse()
